5940da472462d23f3b94d5c280ec948172691c6c
VladimirEronin <vl.eronin@mail.ru>
2022-11-14 17:02:14 +0300

    Add rand

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,7 @@
-﻿int number1 = 38;
-int number2 = 48;
-Console.WriteLine(number1 + number2);
+﻿int number1 = new Random() .Next(1,10);
+int number2 = new Random() .Next(1,10);
+Console.WriteLine("Первое число: " + number1);
+Console.WriteLine("Второе число: " + number2);
+int result = number1 + number2;
+Console.WriteLine("Сумма:"+ result );
+
