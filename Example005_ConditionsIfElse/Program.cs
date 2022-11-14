a2f29682ff1d49d24eb73fb0fc0ec03e6f0dac4b
VladimirEronin <vl.eronin@mail.ru>
2022-11-14 20:46:06 +0300

    add Example005 ConditionsIfElse

---
+++ Example005_ConditionsIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.Write("Ура, это же Маша!!! ");
+}
+else
+{Console.Write("Привет, ");
+Console.Write(username);
+Console.WriteLine("!!!");}
